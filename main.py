ba097926db014425a63c50943964319df0628d20
amaskaraa <155605035+amaskaraa@users.noreply.github.com>
2024-12-14 12:02:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+import streamlit as st
+
+from src.services.process import extract_text, generate_user_stories_and_criteria, \
+    generate_user_stories_and_criteria_specific
+
+
+def main():
+    st.title("BRD Automation System")
+
+    st.sidebar.header("Inputs")
+    brd_file = st.sidebar.file_uploader("Upload BRD Document", type=["pdf", "docx", "txt"])
+    # app_link = st.sidebar.text_input("Application Link")
+    # setup_file = st.sidebar.file_uploader("Upload Setup Guidelines", type=["txt"])
+
+    if st.sidebar.button("Generate Test Cases"):
+        if brd_file is None:
+            st.sidebar.error("Please upload a BRD document before generating test cases.")
+        else:
+            st.subheader("Extracted BRD Requirements")
+            text = extract_text(brd_file)
+
+            st.subheader("Text Evaluation")
+            with st.spinner("Executing test cases..."):
+                user_stories_and_criteria = generate_user_stories_and_criteria(text)
+                user_stories_and_criteria_specific= generate_user_stories_and_criteria_specific(text)
+                st.write(user_stories_and_criteria_specific)
+# ------------------------------------------------------------------------------------------------#
+#         validation_result = validate_test_case(user_stories_and_criteria)
+        #
+        # st.subheader("Test Execution Results")
+        # st.write(validation_result)
+        # if st.button("Generate User Stories and Acceptance Criteria"):
+        #     with st.spinner("Generating user stories and acceptance criteria..."):
+        #         user_stories_and_criteria = generate_user_stories_and_criteria(text)
+        #     st.subheader("Generated User Stories and Acceptance Criteria")
+        #     st.write(user_stories_and_criteria)
+        # print(user_stories_and_criteria)
+        # if setup_file:
+        #     setup_instructions = setup_file.read().decode("utf-8")
+        #     if st.button("Automate Environment Setup"):
+        #         setup_environment(setup_instructions)
+        #         st.success("Environment setup completed successfully!")
+
+        # Generate and Execute Test Cases
+#         if st.button("Generate and Execute Test Cases"):
+#             with st.spinner("Executing test cases..."):
+#                 test_cases = [f"Test case for: {line}" for line in user_stories_and_criteria.splitlines() if line.strip()]
+#                 print(test_cases)
+#                 results = []
+#                 for test_case in test_cases:
+#                     acceptance_criteria = """
+# 1. The platform must integrate all HR functions (Recruitment, Onboarding, Employee Management, Attendance, Leave, PMS, Payroll View, Assets, Offboarding, Helpdesk).
+# 2. Employees must be able to apply for leaves, view/download payslips, regularize attendance, and raise helpdesk tickets.
+# 3. Administrators must configure holidays, leave types, and payroll policies, upload payslips, and define access controls.
+# 4. The setup process for the system must follow guidelines and be automated.
+# 5. The platform must ensure secure, role-based access to data and workflows.
+# """
+#                     validation_result = validate_test_case(test_case, acceptance_criteria)
+#                     status, explanation = validation_result.split("\n", 1)
+#                     results.append({"test_case": test_case, "status": status, "explanation": explanation})
+#             st.subheader("Test Execution Results")
+#             st.write(results)
+#
+#             report_path = generate_report(results)
+#             st.download_button(
+#                 label="Download Test Report",
+#                 data=open(report_path, "rb").read(),
+#                 file_name="test_report.csv",
+#             )
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/services/process.py
@@ -0,0 +1,165 @@
+from PyPDF2 import PdfReader
+from docx import Document
+import openai
+from nltk.tokenize import sent_tokenize
+import os
+import time
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def extract_text(file):
+    """
+    Args:
+        file: Input File Uploaded
+
+    Returns:
+        Contents of the file
+    """
+    if file.type == "application/pdf":
+        pdf_reader = PdfReader(file)
+        text = " ".join(page.extract_text() for page in pdf_reader.pages)
+    elif file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        doc = Document(file)
+        text = "\n".join(paragraph.text for paragraph in doc.paragraphs)
+    else:
+        text = file.read().decode("utf-8")
+    return text
+
+
+def chunk_text(text, max_tokens=3000):
+    """
+    Args:
+        text: Contents fetched from the input file
+        max_tokens: Max token at once
+
+    Returns:
+        Chunks of texts
+    """
+    sentences = sent_tokenize(text)
+    chunks = []
+    current_chunk = []
+    current_length = 0
+
+    for sentence in sentences:
+        sentence_length = len(sentence.split())
+        if current_length + sentence_length > max_tokens:
+            chunks.append(" ".join(current_chunk))
+            current_chunk = []
+            current_length = 0
+
+        current_chunk.append(sentence)
+        current_length += sentence_length
+
+    if current_chunk:
+        chunks.append(" ".join(current_chunk))
+
+    return chunks
+
+def generate_user_stories_and_criteria(text):
+    """
+    Args:
+        text: Contents of the input file
+
+    Returns:
+        Generated user stories and acceptance criteria from the model.
+    """
+    chunks = chunk_text(text)
+    user_stories = []
+
+
+    for chunk in chunks:
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f'''Based on the following requirements:
+             {chunk}
+            Generate user stories and acceptance criteria for each requirement.'''}
+        ]
+        response = openai.ChatCompletion.create(
+        model="gpt-4o",
+        messages=messages,
+        max_tokens=1500,
+        temperature=0
+        )
+        user_stories.append(response.choices[0].message['content'].strip())
+
+    return "".join(user_stories)
+
+def generate_user_stories_and_criteria_specific(text):
+    """
+    Args:
+        text: Contents of the input file
+
+    Returns:
+        Generated user stories and acceptance criteria from the model.
+    """
+    chunks = chunk_text(text)
+    user_stories = []
+
+
+    for chunk in chunks:
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f'''Based on the following requirements:
+             {chunk}
+             Stakeholders has the role for each document.Generate all the user story and acceptance criteria from the document.
+            For Example:
+            1. Recruitment & Onboarding
+            User Story 1: Job Postings
+            As a [role],
+                I want to [goal/action],
+                So that [reason/outcome].
+            
+            Acceptance Criteria:
+            Role: [role]
+                - [action or feature]
+                    - arguments: [parameters, if applicable]
+                - [another action or feature]
+            
+            Ensure all user stories are structured similarly and grouped by functional area (if applicable).'''}
+        ]
+        response = openai.ChatCompletion.create(
+        model="gpt-4o",
+        messages=messages,
+        max_tokens=1500,
+        temperature=0
+        )
+        user_stories.append(response.choices[0].message['content'].strip())
+
+    return "".join(user_stories)
+
+
+# def validate_test_case(user_stories_and_criteria):
+#     """
+#
+#     Args:
+#         user_stories_and_criteria: Takes user stories and acceptance criteria into consideration
+#
+#     Returns:
+#         Test cases with the status and explanation
+#     """
+#     messages = [
+#         {"role": "system", "content": "You are a software testing assistant."},
+#         {"role": "user", "content": f"""
+#     The following contains user stories and their corresponding acceptance criteria:
+#     {user_stories_and_criteria}
+#
+#     For each user story, determine an appropriate test case and validate whether it meets the acceptance criteria.
+#     Respond with a structured list of test cases, their results ('Passed' or 'Failed'), and a brief explanation for each including comments, logs, and any recommendations.
+#     Give it for each user story.Do not miss out on any user story.
+#     """}
+#     ]
+#     response = openai.ChatCompletion.create(
+#         model="gpt-4o",
+#         messages=messages,
+#         max_tokens=3000,
+#         temperature=0
+#     )
+#     return response.choices[0].message['content'].strip()
+
+# def setup_environment(setup_instructions):
+#     with open("setup_script.sh", "w") as f:
+#         f.write(setup_instructions)
+#     os.system("chmod +x setup_script.sh")
+#     os.system("./setup_script.sh")
+
